4d9eda5f84a411873097b0929f1d5e4858044153
Tom Brennan <tommybrennan@gmail.com>
2015-07-12 11:59:58 -0400

    created the if else challenge

---
+++ ruby_challenges/if_else.rb
@@ -0,0 +1,11 @@
+if (1 + 1 == 2)
+  puts "One and one does indeed equal two"
+end
+
+my_name = "Tommy"
+
+if my_name == "Tommy"
+  puts "Helooo, Tommy"
+else
+  puts "Oops, I thought your name was Tommy.  Sorry about that, #{my_name}!"
+end
