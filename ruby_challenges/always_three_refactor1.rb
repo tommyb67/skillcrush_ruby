3fb66fbbda25bafff5b77e434fb0c2548e387ab5
Tom Brennan <tommybrennan@gmail.com>
2015-07-13 22:44:49 -0400

    refactored always three and created the first method file

---
+++ ruby_challenges/always_three_refactor1.rb
@@ -0,0 +1,8 @@
+puts "Give me a number, please! "
+num_one = gets.chomp.to_i
+puts "Thank you for your input of number #{num_one}."
+work_num = (((num_one + 5) * 2) - 4) / 2
+#final_num = work_num - num_one
+puts "Your final number is #{work_num}."
+
+
---
+++ ruby_challenges/always_three_refactor2.rb
@@ -0,0 +1,8 @@
+puts "Give me a number, please! "
+num_one = gets.to_i
+#puts "Thank you for your input of number #{num_one}."
+#work_num = (((num_one + 5) * 2) - 4) / 2
+#final_num = work_num - num_one
+puts "Thank you. Your final number is " + ((((num_one + 5) * 2) - 4) / 2 - num_one).to_s
+
+
