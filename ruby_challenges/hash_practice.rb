9f2bfb0726dbe1a1579843b70f00f00c70e900ba
Tom Brennan <tommybrennan@gmail.com>
2015-07-24 16:17:48 -0400

    fun with hashes

---
+++ ruby_challenges/hash_practice.rb
@@ -0,0 +1,26 @@
+contact_hash_string = {}
+
+contact_hash_string = {"mary" => "mary@example.com"}
+contact_hash_string["tom"] = "tom@example.com"
+contact_hash_string["matt"] = "matt@example.com"
+contact_hash_string["debbie"] = "debbie@example.com"
+
+puts contact_hash_string
+
+contact_hash_symbol_vone = {}
+
+contact_hash_symbol_vone = {:mary => "mary@example.com"}
+contact_hash_symbol_vone[:tom] = "tom@example.com"
+contact_hash_symbol_vone[:matt] = "matt@example.com"
+contact_hash_symbol_vone[:debbie] = "debbie@example.com"
+
+puts contact_hash_symbol_vone
+
+contact_hash_symbol_vtwo = {}
+
+contact_hash_symbol_vtwo = {mary: "mary@example.com"}
+contact_hash_symbol_vtwo [tom: "tom@example.com"]
+contact_hash_symbol_vtwo [matt: "matt@example.com"]
+contact_hash_symbol_vtwo [debbie: "debbie@example.com"]
+
+puts contact_hash_symbol_vone
